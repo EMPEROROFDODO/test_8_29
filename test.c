012d5b52eee856ea547c82732082426631c84855
旭 杨 <3319683684@qq.com>
2024-08-29 23:31:55 +0800

    添加项目文件。

---
+++ test.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>        
+//#include add.h
+//#pragma comment(lib,"test_8_28.lib")
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c= m(a, b);
+//	printf("%d", c);
+//	return 0;
+//}
